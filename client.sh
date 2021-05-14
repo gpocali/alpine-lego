0cd08b6fbf904d7dab39d57676cf03013a231c55
Gregory Pocali <1571781+gpocali@users.noreply.github.com>
2021-05-13 22:49:04 -0400

    Added logging for consistent display and run

--- client.sh
+++ client.sh
@@ -4,17 +4,25 @@
 if [ ! -e /tmp/lego ]; then
     mkdir /tmp/lego
 fi
+if [[ $1 == "firstStart" ]]; then
+    echo -n $(date) - First Start... | tee -a /tmp/legoLog
+    crond -b -l 8 -c /etc/crontabs
+fi
 ng=0
 if [[ "$CERT_SERVER" == "" ]]; then
-    echo Enter the Certificate Server Address in variable CERT_SERVER
+    echo Enter the Certificate Server Address in variable CERT_SERVER | tee -a /tmp/legoLog
     ng=1
 fi
 if [[ "$EMAIL" == "" ]]; then
-    echo Enter the registration email in variable EMAIL
+    echo Enter the registration email in variable EMAIL | tee -a /tmp/legoLog
+    ng=1
+fi
+if [[ "$OUTPUT_FILE" == "" ]]; then
+    echo Enter the output file name in variable OUTPUT_FILE | tee -a /tmp/legoLog
     ng=1
 fi
 if [ $ng -eq 1 ]; then
-    echo Unsatisfied configuration files.  Cannot update certificates
+    echo Unsatisfied configuration files.  Cannot update certificates | tee -a /tmp/legoLog
     exit 0
 fi
 
@@ -26,18 +34,16 @@ else
     exit 0
 fi
 
+echo $(date) - Starting... | tee -a /tmp/legoLog
 domain=$(nslookup $(ifconfig $(route | grep default | awk '{print $8}') | grep "inet addr" | awk '{print $2}' | cut -d: -f2) | grep name | awk '{print $4}')
 certServer=$CERT_SERVER
 email=$EMAIL
-lego -s "$certServer" -a -m "$email" --path /tmp/cert -d "$domain" --http.webroot "/tmp/lego" --http.port 1085 --http --tls --http-timeout 10 renew || \
-lego -s "$certServer" -a -m "$email" --path /tmp/cert -d "$domain" --http.webroot "/tmp/lego" --http.port 1085 --http --tls --http-timeout 10 run
-cat /tmp/cert/certificates/$domain.crt > /output/tower_unraid_bundle.pem
-cat /tmp/cert/certificates/$domain.key >> /output/tower_unraid_bundle.pem
-# Unraid: /boot/config/ssl/certs/tower_unraid_bundle.pem
+lego -s "$certServer" -a -m "$email" --path /tmp/cert -d "$domain" --http.webroot "/tmp/lego" --http.port 1085 --http --tls --http-timeout 10 renew | tee -a /tmp/legoLog || \
+lego -s "$certServer" -a -m "$email" --path /tmp/cert -d "$domain" --http.webroot "/tmp/lego" --http.port 1085 --http --tls --http-timeout 10 run | tee -a /tmp/legoLog
+cat /tmp/cert/certificates/$domain.crt > /output/$OUTPUT_FILE
+cat /tmp/cert/certificates/$domain.key >> /output/$OUTPUT_FILE
+echo -e "$(date) - Finished...\n\n" | tee -a /tmp/legoLog
 
-if [[ $1 == "firstStart" ]]; then
-    echo -n $(date) - First Start...
-    crond -b -l 8 -c /etc/crontabs
-fi
+tail -f /tmp/legoLog
 
 exit 0
